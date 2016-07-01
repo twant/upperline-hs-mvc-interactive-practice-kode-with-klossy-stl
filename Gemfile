569427e467f0f3f17cafb440e6bd9a710e1de644
twant Want <taylor.want@gmail.com>
2016-07-01 16:25:51 +0000

    heroku deploy

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ source "https://rubygems.org"
 gem 'shotgun'
 gem 'pry'
 gem 'sinatra'
-
+gem 'heroku'
--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,18 @@
 require 'bundler'
 Bundler.require
+require_relative "models/pig_latinize.rb"
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
+
+  post '/piglatin' do
+    user_sentence=params[:sentence]
+    @pig_latinized=to_pig_latin(user_sentence)
+    erb :results
+  end
+
 
 end
